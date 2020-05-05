3e517b68d3f3d50c350369ee380e361eae6f8670
Milesq <wmilosz88@gmail.com>
2020-05-05 22:19:15 +0200

    Disable CORS

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, Blueprint
+from flask_cors import CORS
 from dotenv import load_dotenv
 
 from auth import register as auth_blueprint, auth
@@ -7,5 +8,6 @@ from posts import register as posts_blueprint
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 auth_blueprint(app)
 posts_blueprint(app)

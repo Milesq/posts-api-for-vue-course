3ff34623b80800e0712435cd41789dca691559ff
Milesq <wmilosz88@gmail.com>
2020-05-05 15:10:08 +0200

    Add login function

--- auth/login.py
+++ auth/login.py
@@ -1,8 +1,29 @@
-from flask import request, Blueprint
+from flask import request, Blueprint, make_response
+from sqlescapy import sqlescape
+from bcrypt import checkpw
+
+from db_utils import get_db
 
 login_routes = Blueprint('login', __name__)
 
 
 @login_routes.route('/users/login', methods=['POST'])
 def login():
-    return {"token": "secret", "name": request.json}
+    data = request.json
+
+    if 'user' not in data or 'pass' not in data:
+        return make_response({"error": 'User name or password wasn\'t provided'}, 400)
+
+    user, password = sqlescape(data['user']), data['pass'].encode()
+
+    db = get_db()
+    pass_hash = db.execute(
+        f'SELECT pass FROM users WHERE name="{user}"').fetchone()
+
+    if pass_hash == None:
+        return make_response({"error": "User doesn't exists"}, 401)
+
+    if not checkpw(password, pass_hash[0].encode('utf-8')):
+        return make_response({"error": "Password is incorrect"}, 401)
+
+    return {"token": "secret"}

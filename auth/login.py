0009088da3df7f6ec4c823bbdbbf3f5fc473177d
Milesq <wmilosz88@gmail.com>
2020-05-06 15:58:12 +0200

    Show message if private key isn't set

--- auth/login.py
+++ auth/login.py
@@ -6,6 +6,8 @@ from bcrypt import checkpw
 import jwt
 
 from db_utils import get_db
+from util import have
+
 
 login_routes = Blueprint('login', __name__)
 
@@ -14,7 +16,7 @@ login_routes = Blueprint('login', __name__)
 def login():
     data = request.json
 
-    if 'user' not in data or 'pass' not in data:
+    if not have(data, ['user', 'pass']):
         return make_response({"error":
                               'User name or password wasn\'t provided'}, 400)
 
@@ -31,6 +33,10 @@ def login():
         return make_response({"error": "Password is incorrect"}, 401)
 
     secret = os.getenv('PRIVATE_KEY')
+    if secret is None:
+        print('Private key is not set!!!')
+        return make_response({"error": "Private key is not set"}, 500)
+
     encoded_jwt = jwt.encode({'user': user}, secret, algorithm='HS256')
 
     return {"token": encoded_jwt.decode()}
